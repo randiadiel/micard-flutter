7b244d8ccc7a2880c4a1e5ed6b06980f29233712
Randi Adiel Gianufian <randiadiel30@gmail.com>
2020-05-10 21:31:19 +0700

    Column And Row Layout Lesson

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+// You can use EdgeInsets.symetrics/ fromLTRB(vertical: xx.x, horizontal: xx.x)
+//            margin: EdgeInsets.only(left: 30.0),
+//            padding: EdgeInsets.all(20.0),
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              Container(
+                  width: 100.0,
+                  height: 100.0,
+                  color: Colors.blue,
+                  child: Text('Container 2')),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 2'),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
